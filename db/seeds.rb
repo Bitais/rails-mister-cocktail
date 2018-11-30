ee95767028218479b6358092088fda64a658b208
Thibaut de Briey <tdebriey@gmail.com>
2018-11-30 08:50:08 +0000

    create some home page and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed ingredients
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "rhum")
+
+# Seed cocktails
+Cocktail.create(name: "Mojito", photo: 'https://images.unsplash.com/photo-1534409346780-e8af61c48fbc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cb2fc3e06cc903c1a8fee6ce6ef0ebcd&auto=format&fit=crop&w=1500&q=80')
+Cocktail.create(name: "Bloody Mary", photo: 'https://images.unsplash.com/photo-1541546339599-ecdbfcf77378?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9547a12b5517b86ada00074d821f54a8&auto=format&fit=crop&w=1468&q=80')
+Cocktail.create(name: "Gin & Tonic", photo: 'https://images.unsplash.com/photo-1473348229220-66f5e48021f3?ixlib=rb-0.3.5&s=9811c3970eb289afb8006237f5f4d9eb&auto=format&fit=crop&w=1500&q=80')
+Cocktail.create(name: "Whisky Sour", photo: 'https://images.unsplash.com/photo-1541546006121-5c3bc5e8c7b9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c84c763d27b2b813f8c15cfd4bed9ce&auto=format&fit=crop&w=2847&q=80')
+Cocktail.create(name: "Martini", photo: 'https://images.unsplash.com/photo-1438522014717-d7ce32b9bab9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=55cb1a59dd5a3cecf474342bc3048ea7&auto=format&fit=crop&w=1498&q=80')
+Cocktail.create(name: "Pomelo Lou", photo: 'https://images.unsplash.com/photo-1527271982979-83fea3eb3582?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7899bcbd8733fb465132d2ceb2d7dcbf&auto=format&fit=crop&w=976&q=80')
